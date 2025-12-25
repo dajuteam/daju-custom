ab76e62c2263913a036f8d6e55ff982e9754ba2c
dajuteam <dajuteam88@gmail.com>
2025-12-25 17:38:23 +0800

    Update case-list-widget-api-unified.js

--- case-list/case-list-widget-api-unified.js
+++ case-list/case-list-widget-api-unified.js
@@ -384,7 +384,7 @@
     let items = (dataIndex instanceof Map) ? (dataIndex.get(caseName) || []) : [];
 
     if (!items.length) {
-      container.innerHTML = '<div class="case-list-container"><div class="case-list-message">目前尚無上架物件</div></div>';
+      container.innerHTML = '<div class="case-list-container"><div class="case-list-message">- 精選物件上架中 -</div></div>';
       return;
     }
 
